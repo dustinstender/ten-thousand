9a497c4969ce3c106f9d3ebb3882428bd4b55e5a
Dustin Stender <dustinpstender@gmail.com>
2021-08-11 16:22:52 +0200

    comit repos

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,6 @@
 class Post < ApplicationRecord
+  SKILL = %w[HTML CSS Javascript React Ruby Rails]
+
+  validates :skill, inclusion: { in: SKILL, message: 'Please choose proper skill to ensure data is correct.' }
+  validates :hours, presence: true
 end
